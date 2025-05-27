3dbfc41dd105a9f9baef381fdb4361b05cae331a
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2025-05-27 12:59:40 +0500

    update finding command systems and add skills...

---
+++ skills/times/skill.py
@@ -0,0 +1,9 @@
+def get_time(*args):
+    """
+    Функция для получения текущего времени по команде.
+    :param args: Дополнительные аргументы (не используются)
+    :return: Строка с текущим временем
+    """
+    from datetime import datetime
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return f"Текущее время: {current_time}"  # Возвращаем текущее время в формате ЧЧ:ММ:СС.
